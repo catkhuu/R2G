a143e1e37d4b50fa2f83de51abf6b460d10024b6
Lindsay Kelly <email": null>
2016-11-23 21:26:09 -0500

    Add profile new/create and show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,23 @@
 class ProfilesController < ApplicationController
+  before_action :sanitize_profile_params, only: [:create, :update]
 
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    debugger
+    # @profile.user = current_user
+    if @profile.save
+      redirect_to user_profile_path(current_user, @profile)
+    else
+      render 'new'
+    end
   end
 
   def show
+    @profile = current_user.profile
   end
 
   def edit
@@ -17,6 +28,11 @@ class ProfilesController < ApplicationController
 
   private
     def profile_params
-      params.require(:profile).permit(:why_i_run, :user_pace, :goals, :experience, :need_to_knows)
+      params.require(:profile).permit(:why_i_run, :user_pace, :goals, :user_id, :experience, :need_to_knows)
+    end
+
+    def sanitize_profile_params
+      # params['profile']['experience'] = params['experience']
+      params['profile']['user_id'] = current_user.id
     end
 end
