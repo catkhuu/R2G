f7eb0b80141583b85aa58472616e68aae2c2f8ed
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-25 10:13:31 -0500

    Convert user's zipcode to city with geocode_zip method in users_controller. geocode_zip is called in a before_action filter for create and update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,29 @@
-
+require 'unirest'
 class UsersController < ApplicationController
+    before_action :geocode_zip, only: [:create, :update]
+    before_action :logged_in_user, :find_and_ensure_user, only: [:show, :edit, :update]
+    before_action :correct_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user
+    binding.pry
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
+      @errors = user.errors.full_messages
       render 'new'
     end
   end
 
   def show
-    find_user
   end
 
   def edit
-    find_user
   end
 
   def update
@@ -29,11 +31,22 @@ class UsersController < ApplicationController
   end
 
   private
-    def find_user
-      @user = User.find(params[:id])
+    def find_and_ensure_user
+      render 'application/error_404' unless @user = User.find_by(id: params[:id])
     end
 
     def user_params
       params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
+    end
+
+    def correct_user
+      render 'application/error_404' unless current_user?
+    end
+
+    def geocode_zip
+      url = "http://maps.googleapis.com/maps/api/geocode/json?"
+      parameters = { address: params[:user][:location], sensor: 'true' }
+      response = Unirest.get(url, headers: {}, parameters: parameters)
+      params[:user][:location] = response.body['results'][0]['address_components'][1]['short_name']
     end
 end
