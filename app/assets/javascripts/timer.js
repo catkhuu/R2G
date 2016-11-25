a9b131ba1935ee75a44a60f93158119605626ec3
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-25 15:10:16 -0500

    Timer counts up and starts and stops when button is clicked.

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,5 +1,5 @@
 $(function() {
-  $(".example.stopwatch").TimeCircles({ time: { Days: { show: false }, Hours: { show: false } }});
+  $(".example.stopwatch").TimeCircles( {start: false}, { time: { Days: { show: false }, Hours: { show: false } }});
   $(".start").click(function(){ $(".example.stopwatch").TimeCircles().start(); });
   $(".stop").click(function(){ $(".example.stopwatch").TimeCircles().stop(); });
   $(".restart").click(function(){ $(".example.stopwatch").TimeCircles().restart(); });
