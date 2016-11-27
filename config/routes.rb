a3428f9f553b01742ac8b89a6ec6e732563a3a6c
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-27 15:44:04 -0500

    Add upvotes/declines routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
+  put '/runs/:id/upvotes', to: 'runs#upvotes'
+  put '/runs/:id/declines', to: 'runs#declines'
   resources :users, except: [:index, :destroy] do
     resources :profiles, except: [:index, :destroy]
     resources :moods, except: [:show]
