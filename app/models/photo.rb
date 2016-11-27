f3d91792a742628564f712a7c32bcfdbca1be3cc
Miles McArdle-Coe <email": null>
2016-11-26 19:32:58 -0500

    remove old photo class and related files

--- app/models/photo.rb
+++
@@ -1,6 +0,0 @@
-class Photo < ApplicationRecord
-  belongs_to :user
-  has_attached_file :image
-  validates_attachment :image,
-                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-end
